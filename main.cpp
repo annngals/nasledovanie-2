0587da3da97df1d8fe4a9865fb721f0ee52aa9d3
annngals <57696371+annngals@users.noreply.github.com>
2020-06-06 12:46:45 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <locale.h>
+#include <vector>
+#include "human.h"
+#include "student.h"
+
+using namespace std;
+
+int main(int atgc, char* argv[]){
+	vector<int> scores;
+	setlocale(LC_ALL, "rus");
+
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(2);
+	scores.push_back(2);
+	scores.push_back(5);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+	scores.push_back(3);
+
+	student *stud = new student("Петров", "Иван", "Алексеевич", scores);
+
+	cout << stud->get_full_name() << endl;
+	cout << "Средний балл: " << stud->get_averaged_score() << endl;
+	system("pause");
+	return 0;
+}
